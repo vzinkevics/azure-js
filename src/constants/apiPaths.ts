077e5bf5a96d7d19edc2476936b8cc6af7f6676f
v.zinkevics <vadims.zinkevics@livewire.com>
2023-12-20 20:05:24 +0200

    task 3

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -2,7 +2,7 @@ const API_PATHS = {
   product: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  bff: "https://products-service-apim.azure-api.net/test/api",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
 
--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -12,17 +12,17 @@ export const handlers = [
   rest.put(`${API_PATHS.bff}/product`, (req, res, ctx) => {
     return res(ctx.status(200));
   }),
-  rest.delete(`${API_PATHS.bff}/product/:id`, (req, res, ctx) => {
+  rest.delete(`${API_PATHS.bff}/products/:id`, (req, res, ctx) => {
     return res(ctx.status(200));
   }),
-  rest.get(`${API_PATHS.bff}/product/available`, (req, res, ctx) => {
+  rest.get(`${API_PATHS.bff}/products`, (req, res, ctx) => {
     return res(
       ctx.status(200),
       ctx.delay(),
       ctx.json<AvailableProduct[]>(availableProducts)
     );
   }),
-  rest.get(`${API_PATHS.bff}/product/:id`, (req, res, ctx) => {
+  rest.get(`${API_PATHS.bff}/products/:id`, (req, res, ctx) => {
     const product = availableProducts.find((p) => p.id === req.params.id);
     if (!product) {
       return res(ctx.status(404));

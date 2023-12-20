20e3290d751f2ad71dc5283227ca291d2ce51eb8
v.zinkevics <vadims.zinkevics@livewire.com>
2023-12-20 20:10:57 +0200

    task 3

--- infra/main.tf
+++ infra/main.tf
@@ -32,35 +32,3 @@ resource "azurerm_storage_account" "front_end_storage_account" {
     index_document = "index.html"
   }
 }
-
-
-#variable "subscription_key" {
-#  type      = string
-#  sensitive = true
-#}
-
-resource "azurerm_app_configuration" "app_config" {
-  name                = "appConfig103"
-  resource_group_name = azurerm_resource_group.front_end_rg.name
-  location            = azurerm_resource_group.front_end_rg.location
-  sku                 = "free"
-}
-
-#data "azurerm_client_config" "current" {}
-
-#resource "azurerm_role_assignment" "appconf_dataowner" {
-#  scope                = azurerm_app_configuration.app_config.id
-#  role_definition_name = "App Configuration Data Owner"
-#  principal_id         = data.azurerm_client_config.current.object_id
-#}
-
-#resource "azurerm_app_configuration_key" "apim_subscription_key" {
-#  configuration_store_id = azurerm_app_configuration.app_config.id
-#  label                  = "APIM subscription key"
-#  key                    = "APIM_SUBSCRIPTION_KEY"
-#  value                  = var.subscription_key
-#
-#  depends_on = [
-#    azurerm_role_assignment.appconf_dataowner
-#  ]
-#}

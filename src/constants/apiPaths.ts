cf8bd9d5779b1242e43d3a7fc13787a6e9db3919
v.zinkevics <vadims.zinkevics@livewire.com>
2024-02-07 11:27:50 +0200

    task 5

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
   product: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  import: "https://products-service-apim.azure-api.net/container/api",
   bff: "https://products-service-apim.azure-api.net/test/api",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
